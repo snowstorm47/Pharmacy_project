1300b8976a7f0487b5d3d6e0654e79f825eb6cb5
Yakob Dereje <yakobdereje.yd@gmail.com>
2024-05-15 11:18:24 -0700

    Quick Info cards 2nd day work

---
+++ PP_team_fiv/lib/dashboard/domain/models/dashboard_cards_modes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class quickInfoData {
+  final Icon icon;
+  final String value;
+  final String title;
+
+  const quickInfoData(
+      {required this.icon, required this.value, required this.title});
+}
