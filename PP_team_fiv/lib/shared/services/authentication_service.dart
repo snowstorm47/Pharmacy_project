27d419e1c0e9ff98122613b87ec85ff27ff59d13
yordanos-habtamu <jordrichh7@gmail.com>
2024-05-16 09:40:38 +0300

    added the signin service with authentication and email verification

---
+++ PP_team_fiv/lib/shared/services/authentication_service.dart
@@ -0,0 +1,76 @@
+
+import 'package:firebase_auth/firebase_auth.dart';//importing firebase authentication
+import 'package:cloud_firestore/cloud_firestore.dart';//importing firebase firestore
+
+import '../models/puser.dart';//importing the user model
+
+
+class SigninService{
+  //firebase instances
+  final FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firebaseFirestore;
+  SigninService(this._firebaseAuth,this._firebaseFirestore);
+  
+//Signin function returns a user object
+Future<PUser?> SigninUser(  {required email , required password})async{
+  try{
+    UserCredential result = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    //using firebase auth with email and password
+    User user = result.user!; // returns firebse user
+    
+    print(user);
+   final docSnapshot =await getData(user.uid); //gets the document which has the user id
+   if(docSnapshot != null){
+    //loading the userdata from the firebase document to the  user object
+   final userData =  PUser(
+       access : docSnapshot?.data()?['access'],
+     permission :docSnapshot?.data()?['permission'],
+     photoUrl: docSnapshot?.data()?['photoUrl'],
+     role :docSnapshot?.data()?['role'],
+     name :docSnapshot?.data()?['name'],
+     branch : docSnapshot?.data()?['branch'],
+     email :docSnapshot?.data()?['email'],
+     uid:docSnapshot?.data()?['uid'],
+   );
+   print(userData);
+   return userData;
+   
+   }
+   else{
+    print('there is no snapshot');
+    return null;
+   }
+  } on FirebaseAuthException catch (e) {
+  if (e.code == 'user-not-found') {
+    print('No user found for that email.');
+  } else if (e.code == 'wrong-password') {
+    print('Wrong password provide');
+    }
+    }
+  return null;
+}
+//fuction obtaining the document using the user id
+Future<DocumentSnapshot<Map<String, dynamic>>?> getData(String uid) async {
+    final DocRef= _firebaseFirestore.collection('users').doc('user.id');
+   final docSnapshot = await DocRef.get();
+  if (docSnapshot.exists) {
+    return docSnapshot;
+  } else {
+    print("Document does not exist");
+    return null;
+  }
+}
+Future<void> verifyEmail() async{
+  User? user = _firebaseAuth.currentUser;
+  if (user!= null && !user.emailVerified) {
+  await user.sendEmailVerification();
+}
+ else {
+  return;
+ }
+
+}
+Future<void> Signout() async{
+ await _firebaseAuth.signOut();
+}
+}
