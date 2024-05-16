12e828fa782b1ce273cfef2e6e337921867a656d
Yohannes Dereje <yohannesdereje1221@gmail.com>
2024-05-16 11:29:53 -0700

    had to start over

--- PP_team_fiv/lib/settings/settingsNav/lib/const/constant.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const cardBackgroundColor = Color.fromARGB(0, 255, 255, 255);
-const primaryColor = Color(0x7193E9);
-const fontColor = Color(0x0B054C);
-const inputableColor = Color(0xF1EFEF);
-
-const defaultPadding = 20.0;
