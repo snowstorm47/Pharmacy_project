6c3e3456e70196563d209ca004f57f0a0214c7f4
sitra jemal <winj1974@gmail.com>
2024-05-17 21:08:54 +0300

    piechart widget

--- PP_team_fiv/lib/main.dart
+++ PP_team_fiv/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:clean_a/dashboard/presentation/widgets/barchart_widget.dart';
+import 'package:clean_a/dashboard/presentation/widgets/piechart_widget.dart';
 import 'package:clean_a/header.dart';
 import 'package:clean_a/login_p/presentation/new_p.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,15 +15,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Define the data for the pie chart
+    final Map<String, double> dataMap = {
+      'A': 5,
+      'B': 3,
+      'C': 2,
+       'D': 3,
+      'E': 2,
+    };
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: //new_pPage(),
-          Header(),
+      home: Pie_chart(dataMap: dataMap),
+            //new_pPage()
+           // Header()
     );
   }
 }
