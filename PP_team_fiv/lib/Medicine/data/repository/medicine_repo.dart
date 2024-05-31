fd72b740a909537317bd9e4fc0c19f2566fc1735
yordanos-habtamu <jordrichh7@gmail.com>
2024-05-31 13:58:47 +0300

    some changes to the entities and the remove branch function

--- PP_team_fiv/lib/Medicine/data/repository/medicine_repo.dart
+++ PP_team_fiv/lib/Medicine/data/repository/medicine_repo.dart
@@ -1,3 +1,4 @@
+import 'package:app/Medicine/domain/entities/batch.dart';
 import 'package:app/Medicine/domain/entities/medicine.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -21,27 +22,37 @@ Future<void>  AddMedicine(
     required Taxable,
     required PrescriptionBased,
     required Details,
+    required balance,
+    required DateTime dateAdded
   }
   )async{
   
   final CollectionReference MedicineRef = _firebaseFirestore.collection('Medicine');
-  final medicineId = MedicineRef.doc().id;
-  final MedicineData = Medicine(
-    Location:Location,
-    MedicineName:MedicineName,
-    MedicineId:medicineId,
-    Catagory:Catagory,
-    weight:weight,
-    GenericName:GenericName,
-    SuppliersPrice:SuppliersPrice,
-     SellingPrice:SellingPrice,
-    ExpiryDate:ExpiryDate,
-    Taxable:Taxable,
-    PrescriptionBased:PrescriptionBased,
-     Details:Details,
-     Stock:Stock
-  );
-  await MedicineRef.doc(medicineId).set(MedicineData.toMap());
+  final docName = MedicineName;
+  final  List<Batch>batchList=[];
+  final BatchNumber= dateAdded.toString();
+  final batch= Batch(
+    Location: Location,
+     BatchNumber:BatchNumber, 
+     ExpiryDate:ExpiryDate,
+      dateAdded: dateAdded, 
+      Stock: Stock
+           );
+     batchList.add(batch);
+    final Med = Medicine(
+      Catagory: Catagory,
+       MedicineName: MedicineName,
+        balance: balance,
+         weight:weight, 
+         batch: batchList,
+          Details: Details,
+           GenericName:GenericName,
+            PrescriptionBased: PrescriptionBased,
+             SellingPrice: SellingPrice, 
+             SuppliersPrice: SuppliersPrice,
+              Taxable: Taxable,     
+    );
+      await MedicineRef.doc(docName).set(Med);
   }
   Future<void> deleteMedicince({
     required MedicineName,
@@ -66,5 +77,32 @@ Future<void>  AddMedicine(
     print(e.toString());
   }
   }
+Future<void> removeBatch({required MedicineName,required BatchNumber}) async {
+  final MedRef = _firebaseFirestore.collection('medicine').doc(MedicineName);
+  final batchNum = BatchNumber;
+  final transaction = await _firebaseFirestore.runTransaction((transaction)async{
+ final docSnapshot = await transaction.get(MedRef);
+  if(docSnapshot.exists){
+final data = docSnapshot.data();
+if(data!=null){
+  if(data.containsKey('batch')){
+    final List<Batch> batchList = List.from(data['batch'] as List<Batch>);
+    batchList.removeWhere((batch)=>batch.BatchNumber== batchNum);
+    transaction.update(MedRef,{'batch':batchList});
+  }else{
+    print("the doc doesn't have the key batch");
+  }
+}
+  }else{
+    print('there is no file');
+  }
+  }); 
+ if(transaction!=null){
+  print('batch remove successfully');
+ }
+ else{
+  print('falied to remove batch');
+ }
+}
 
 }
