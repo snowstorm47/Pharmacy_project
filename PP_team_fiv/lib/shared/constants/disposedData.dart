918c6fe2a4b3ee8def1e43bde87b01877a7a747a
Betelhem <elbetelmisgan17@gmail.com>
2024-06-14 00:39:42 +0300

    Import constants from sitra's branch

---
+++ PP_team_fiv/lib/shared/constants/disposedData.dart
@@ -0,0 +1,18 @@
+class DataDis {
+  final String Batch;
+  final String ProductName;
+  final String Catagory;
+  final String Price;
+  final String Date;
+final String ExpiryDate;
+  DataDis({
+    required this.Batch,
+    required this.ProductName,
+    required this.Catagory,
+        required this.Price,
+    required this.Date,
+    required this.ExpiryDate, 
+  });
+
+  
+}
---
+++ PP_team_fiv/lib/shared/constants/meddata.dart
@@ -0,0 +1,27 @@
+
+class DataMD {
+  final String medicineName;
+  final String Id;
+  final String GenericName;
+  final String Catagory;
+  final String Weight;
+  final String expiryDate;
+  final  bool isTaxable;
+  final bool  isPrescribed;
+
+
+  DataMD({
+    required this.medicineName,
+    required this.Id,
+    required this.GenericName,
+    required this.expiryDate,
+    required this.Weight,
+   required this.Catagory, 
+ required this.isTaxable,
+   required this.isPrescribed,
+  });
+
+ 
+
+  
+}
