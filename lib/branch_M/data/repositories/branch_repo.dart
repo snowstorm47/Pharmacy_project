f19b8a57eda08d3b4fe807b58c68c479f9985acd
yordanos-habtamu <jordrichh7@gmail.com>
2024-07-11 22:09:35 +0300

    tweaked some lines

--- lib/branch_M/data/repositories/branch_repo.dart
+++ lib/branch_M/data/repositories/branch_repo.dart
@@ -68,6 +68,7 @@ class RegisterBranch{
     if(snapShot!= null){
       return Branch.fromMap(snapShot.data() as Map<String,dynamic>);
     }
+    return null;
   }
  
  Future<void> addRefill({  required branchName, required refillRequest,required requestDate, required requestedBy,}) async{
@@ -80,10 +81,33 @@ class RegisterBranch{
   );
   await refillRef.doc().set(refill.toMap());
  }
-  
    Future <List<Object>?>getRefill()async{
      final CollectionReference refillCollection= _firebaseFirestore.collection('refill');
     final snapshot = await refillCollection.get();
    return snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
   }
+   Future<void> removeRefill(id)async{
+     final CollectionReference refillCollection= _firebaseFirestore.collection('refill');
+    await refillCollection.doc(id).delete();  
+ }
+ Future<void> editRefill(String id, {required Map<String, dynamic> updatedData}) async {
+  final collectionReference = _firebaseFirestore.collection('refill');
+  final document = collectionReference.doc(id);
+
+  // Transaction for data consistency
+  await _firebaseFirestore.runTransaction((transaction) async {
+    final documentSnapshot = await transaction.get(document);
+
+    if (!documentSnapshot.exists) {
+      throw Exception('Request Not Found not found'); // Handle non-existent employee
+    }
+
+    // Update only specified fields (prevents overwriting entire document)
+    final existingData = documentSnapshot.data()!;
+    existingData.updateAll((key, value) => updatedData.containsKey(key) ? updatedData[key] : value);
+
+    transaction.set(document, existingData);
+  });}
+
+
 }
