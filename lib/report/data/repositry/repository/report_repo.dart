3caec59df04345f536fa7c9298875f34626c8865
yordanos-habtamu <jordrichh7@gmail.com>
2024-07-18 01:07:21 +0300

    added the report provider

--- lib/report/data/repositry/repository/report_repo.dart
+++ lib/report/data/repositry/repository/report_repo.dart
@@ -1,4 +1,6 @@
 
+import 'package:clean_a/finance/domain/entities/expense.dart';
+import 'package:clean_a/finance/domain/entities/income.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ReportRepo{
@@ -6,16 +8,21 @@ FirebaseFirestore _firebaseFirestore= FirebaseFirestore.instance;
 
 
 
-Future<void> generateSalesReport(DateTime startingDate,DateTime endTime) async{
+Future<List<Income>?> generateSalesReport(DateTime startingDate,DateTime endTime) async{
  
  final invoiceRef = _firebaseFirestore.collection('income');
  final query = invoiceRef.where('createdAt',isGreaterThanOrEqualTo: startingDate,isLessThanOrEqualTo:endTime);
  final snapshot= await query.get();
-  
+return snapshot.docs.map((doc) {
+    return Income.fromMap(doc.data() as Map<String, dynamic>);
+  }).toList();
 }
-Future<void> generatePurchaseReport(DateTime startingDate,DateTime endTime) async{
+Future<List<Expense>?> generatePurchaseReport(DateTime startingDate,DateTime endTime) async{
 final invoiceRef = _firebaseFirestore.collection('expense');
  final query = invoiceRef.where('createdAt',isGreaterThanOrEqualTo: startingDate,isLessThanOrEqualTo:endTime);
  final snapshot= await query.get();
+return snapshot.docs.map((doc) {
+    return Expense.fromMap(doc.data() as Map<String, dynamic>);
+  }).toList();
 }
 }
